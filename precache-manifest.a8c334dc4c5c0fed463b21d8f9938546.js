2afb237675e57766784cdee3f3e46f2af7c8d18c
Mike <troianello@protonmail.com>
2019-10-01 18:37:17 -0400

    Updates

---
+++ precache-manifest.a8c334dc4c5c0fed463b21d8f9938546.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d5bc5e3a38eb9163c2adc4068ab348",
+    "url": "/troianello/index.html"
+  },
+  {
+    "revision": "6a93d3707f34d86a3b34",
+    "url": "/troianello/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "a48cabebc9f21bfa7062",
+    "url": "/troianello/static/css/main.20d544dd.chunk.css"
+  },
+  {
+    "revision": "6a93d3707f34d86a3b34",
+    "url": "/troianello/static/js/2.1f5dfb05.chunk.js"
+  },
+  {
+    "revision": "a48cabebc9f21bfa7062",
+    "url": "/troianello/static/js/main.4aab69b1.chunk.js"
+  },
+  {
+    "revision": "73a103bd20ffb1f72ae7",
+    "url": "/troianello/static/js/runtime~main.3af5dd8a.js"
+  },
+  {
+    "revision": "3f8e68f49f7dba3390a43f8fcc1a14f7",
+    "url": "/troianello/static/media/css3.3f8e68f4.png"
+  },
+  {
+    "revision": "e7256fcc694d1fece0727bca2f4051e8",
+    "url": "/troianello/static/media/express2.e7256fcc.png"
+  },
+  {
+    "revision": "7f130501681cb3d0129f266820c78b24",
+    "url": "/troianello/static/media/javascript.7f130501.png"
+  },
+  {
+    "revision": "b3c5133feb48eaaf72ed89f1565a622a",
+    "url": "/troianello/static/media/jquery.b3c5133f.png"
+  },
+  {
+    "revision": "ecc6f9e5d804ae0db7490c683af2f406",
+    "url": "/troianello/static/media/mongoDB2.ecc6f9e5.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/troianello/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "084f0cf8b8a5fdff8203d6264b1dd700",
+    "url": "/troianello/static/media/sass.084f0cf8.png"
+  }
+]);
